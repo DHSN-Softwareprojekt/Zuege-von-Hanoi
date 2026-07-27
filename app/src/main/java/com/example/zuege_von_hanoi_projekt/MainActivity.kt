2620fd587388004f3cde3b683221dfb4b06145ec
I767493 <marwin.lohmann@sap.com>
2026-07-27 10:55:33 +0200

    initialize Kotlin App

---
+++ app/src/main/java/com/example/zuege_von_hanoi_projekt/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.zuege_von_hanoi_projekt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.zuege_von_hanoi_projekt.ui.theme.ZuegevonHanoiProjektTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ZuegevonHanoiProjektTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ZuegevonHanoiProjektTheme {
+        Greeting("Android")
+    }
+}
